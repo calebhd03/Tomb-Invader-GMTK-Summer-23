8a53a65297253ddbfd08a52d5b9af1077642e3e3
calebhd03 <calebhd03@gmail.com>
2023-07-07 15:03:30 -0400

    imported text mesh pro

--- Assets/ItemSlotManager.cs
+++ Assets/ItemSlotManager.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class ItemSlotManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] List<ItemSlot> slots;
+    [SerializeField] List<Item> items;
+
+    private void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FillItemSlots()
     {
-        
+        foreach(ItemSlot slot in slots)
+        {
+
+        }
     }
+
 }
