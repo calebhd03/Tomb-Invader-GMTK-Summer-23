44dda5ce20dbfc36ad120cc7facae0fbfa41f8d9
calebhd03 <calebhd03@gmail.com>
2023-07-08 14:56:34 -0400

    started on loss tracker

--- Assets/UI/LossTracker/LossTracker.cs
+++ Assets/UI/LossTracker/LossTracker.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LossTracker : MonoBehaviour
 {
+    [SerializeField] PlayerStats playerStats;
+
+    [SerializeField] List<TextMeshProUGUI> lossCounters;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateLossTracker();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateLossTracker()
     {
-        
+
     }
 }
