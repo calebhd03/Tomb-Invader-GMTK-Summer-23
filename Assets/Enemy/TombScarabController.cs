fc7d7880d5c4ef70b2cce77819b2f23c2455a2ca
SkylightTheFox <ItsNotSkylight.539@gmail.com>
2023-07-08 14:49:42 -0400

    added enemy state machine for tomb scarab

--- Assets/Enemy/TombScarabController.cs
+++ Assets/Enemy/TombScarabController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public enum ScarabState
 {
+    stop,
+    idle,
     pursue,
     attack,
     knockedBack,
@@ -16,19 +18,30 @@ public class TombScarabController : MonoBehaviour
     /// Mindless pawns that just surround player
     /// Also includes simple finite state machine
     /// Manages state of monster and tells it what to do and when to do it
+    /// Adjust values in editor
     /// </summary>
 
     // Scarab Components
     public ScarabState state;
     public Animator animator;
 
-    // Attack Variables
+    
     public float chaseDistance;
-    public float attackDistance;
+    public float chaseSpeed;
 
-    // Pursuit
+    public float attackDistance;
+    public float attackSpeed;
+    public float arcHeight;
+    
+    public float startTime;
+
+    public float idleSpeed;
+    public float idleDistance;
+    
     private Vector3 direction;
     private Vector3 destination;
+    private Vector3 cleared = new Vector3(-1, -1, -1);
+    private Vector3 start;
 
     // Player References
     private float distanceToPlayer;
@@ -36,7 +49,7 @@ public class TombScarabController : MonoBehaviour
 
     void Start()
     {
-        state = ScarabState.pursue;
+        state = ScarabState.idle;
         playerTransform = GameObject.FindObjectOfType<Player>().transform;
     }
 
@@ -44,16 +57,78 @@ public class TombScarabController : MonoBehaviour
     {
         distanceToPlayer = Vector2.Distance(playerTransform.position, transform.position);
 
-        if (distanceToPlayer < chaseDistance && distanceToPlayer > attackDistance && state == ScarabState.pursue)
+        if (distanceToPlayer < chaseDistance && distanceToPlayer > attackDistance && state == ScarabState.idle)
         {
             state = ScarabState.pursue;
             destination = playerTransform.position;
             animator.Play("move");
         }
+        else if (distanceToPlayer < attackDistance && (state == ScarabState.idle || state == ScarabState.pursue))
+        {
+            state = ScarabState.stop;
+            start = transform.position;
+            direction = (playerTransform.position - start).normalized;
+            destination = start + (direction * attackDistance);
+            animator.Play("attack");
+        }
+        else if (distanceToPlayer > attackDistance && destination == cleared)
+        {
+            state = ScarabState.idle;
+            animator.Play("idle");
+            destination = new Vector3(transform.position.x + Random.Range(idleDistance * -1, idleDistance + 2), transform.position.y + Random.Range(idleDistance * -1, idleDistance + 2), 0f);
+
+        }
+        
+        if (state == ScarabState.idle)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, destination, idleSpeed);
+        }
         else if (state == ScarabState.attack)
         {
-            // Calculate if player is in proper range
-            // Execute attack code here or in another script?
+            // The movement behind the attack...?
+
+            Vector3 center = (start + destination) * 0.5f;
+
+            center -= new Vector3(0, arcHeight, 0);
+
+            Vector3 startRelCenter = start - center;
+            Vector3 destinationRelCenter = destination - center;
+
+            float fractionComplete = (Time.time - startTime / 2.0f);
+
+            transform.position = Vector3.Slerp(startRelCenter, destinationRelCenter, fractionComplete);
+            playerTransform.position += (Vector3)center;
         }
+        else if (state == ScarabState.pursue)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, destination, chaseSpeed);
+        }
+
+
+
+        if (state != ScarabState.idle || destination == cleared)
+        {
+            state = ScarabState.idle;
+            animator.Play("idle");
+            destination = new Vector3(transform.position.x + Random.Range(idleDistance * -1, idleDistance + 2), transform.position.y + Random.Range(idleDistance * -1, idleDistance + 2), 0f);
+        }
+
+    }
+
+    void SetToStop()
+    {
+        state = ScarabState.stop;
+    }
+
+    void SetToIdle()
+    {
+        state = ScarabState.idle;
+        destination = cleared;
+    }
+
+    void StartAttack()
+    {
+        state = ScarabState.attack;
+        startTime = Time.time;
     }
 }
