53f72d97be170c9adf700d77a7ee32cb188f0b4d
calebhd03 <calebhd03@gmail.com>
2023-07-09 11:51:29 -0400

    Added win and lose text

---
+++ Assets/QuitOnButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOnButton : MonoBehaviour
+{
+    public void QuitTheApplication()
+    {
+        Application.Quit();
+    }
+}
